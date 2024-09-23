d67d09e82924993c84d29b528257e53d33bed142
Andrew Drogalis <108765079+drogalis@users.noreply.github.com>
2024-09-23 12:12:11 -0400

    Micro-performance boost
    - Added std::memory_order_relaxed
    - Unaligned std::atomic<std::size_t> turn
    - Complete size();

--- include/dro/mpmc-queue.hpp
+++ include/dro/mpmc-queue.hpp
@@ -1,4 +1,3 @@
-
 // Andrew Drogalis Copyright (c) 2024, GNU 3.0 Licence
 //
 // Inspired from Erik Rigtorp
@@ -9,6 +8,8 @@
 //    Manual allocation is the most straight forward solution.
 // 3) Used copy assignment instead of placement new
 // 4) Added C++20 concepts
+// 5) Removed turn alignment
+// 6) Added std::memory_order_relaxed for atomic fetch_add
 
 #ifndef DRO_MPMC_QUEUE
 #define DRO_MPMC_QUEUE
@@ -17,6 +18,7 @@
 #include <concepts>
 #include <cstddef>
 #include <cstdint>
+#include <cstdlib>
 #include <limits>
 #include <new>// for std::hardware_destructive_interference_size
 #include <stdexcept>
@@ -47,8 +49,10 @@ concept MPMC_NoThrow_Type =
 
 template <MPMC_Type T> struct alignas(cacheLineSize) Slot
 {
-  alignas(cacheLineSize) std::atomic<std::size_t> turn {0};
   T data_ {};
+  // This should not be aligned. Due to the aligned struct this doesn't cause
+  // cache contention. Alignment here would increase memory footprint.
+  std::atomic<std::size_t> turn {0};
 
   Slot() = default;
   ~Slot()
@@ -119,9 +123,7 @@ private:
       std::numeric_limits<std::size_t>::max();
 
   alignas(cacheLineSize) std::atomic<std::size_t> tail_ {0};
-  alignas(cacheLineSize) std::size_t tailCache_ {0};
   alignas(cacheLineSize) std::atomic<std::size_t> head_ {0};
-  alignas(cacheLineSize) std::size_t headCache_ {0};
 
   [[nodiscard]] std::size_t turn(size_t index) const noexcept
   {
@@ -144,7 +146,7 @@ public:
       capacity_ = MAX_SIZE_T - 1;
     }
     // ++capacity_;// prevents live lock e.g. reader and writer share 1 slot for
-                // size 1
+    // size 1
     buffer_ = allocator_.allocate(capacity_ + 1);
     for (size_t i {}; i < capacity_; ++i) { new (&buffer_[i]) Slot<T>(); }
   }
@@ -165,7 +167,7 @@ public:
     requires std::constructible_from<T, Args...>
   void emplace(Args&&... args) noexcept(MPMC_NoThrow_Type<T, Args&&...>)
   {
-    auto const head = head_.fetch_add(1);
+    auto const head = head_.fetch_add(1, std::memory_order_relaxed);
     auto& slot      = buffer_[head % capacity_];
     while (turn(head) * 2 != slot.turn.load(std::memory_order_acquire)) {}
     slot.assign_value(std::forward<Args>(args)...);
@@ -174,7 +176,8 @@ public:
 
   template <typename... Args>
     requires std::constructible_from<T, Args...>
-  bool try_emplace(Args&&... args) noexcept(MPMC_NoThrow_Type<T, Args&&...>)
+  [[nodiscard]] bool try_emplace(Args&&... args) noexcept(
+      MPMC_NoThrow_Type<T, Args&&...>)
   {
     auto head = head_.load(std::memory_order_acquire);
     while (true)
@@ -210,28 +213,28 @@ public:
     emplace(std::forward<P>(val));
   }
 
-  bool try_push(const T& val) noexcept(MPMC_NoThrow_Type<T>)
+  [[nodiscard]] bool try_push(const T& val) noexcept(MPMC_NoThrow_Type<T>)
   {
     return try_emplace(val);
   }
 
   template <typename P>
     requires std::constructible_from<T, P&&>
-  bool try_push(P&& val) noexcept(MPMC_NoThrow_Type<T, P&&>)
+  [[nodiscard]] bool try_push(P&& val) noexcept(MPMC_NoThrow_Type<T, P&&>)
   {
     return try_emplace(std::forward<P>(val));
   }
 
   void pop(T& val) noexcept
   {
-    auto const tail = tail_.fetch_add(1);
+    auto const tail = tail_.fetch_add(1, std::memory_order_relaxed);
     auto& slot      = buffer_[tail % capacity_];
     while (turn(tail) * 2 + 1 != slot.turn.load(std::memory_order_acquire)) {}
     val = slot.return_value();
     slot.turn.store(turn(tail) * 2 + 2, std::memory_order_release);
   }
 
-  bool try_pop(T& val) noexcept
+  [[nodiscard]] bool try_pop(T& val) noexcept
   {
     auto tail = tail_.load(std::memory_order_acquire);
     while (true)
@@ -258,24 +261,24 @@ public:
     }
   }
 
-  [[nodiscard]] std::size_t size() const noexcept
+  [[nodiscard]] std::ptrdiff_t size() const noexcept
   {
-    std::ptrdiff_t diff = head_.load(std::memory_order_acquire) -
-                          tail_.load(std::memory_order_acquire);
-    if (diff < 0)
-    {
-      diff += capacity_;
-    }
-    return static_cast<std::size_t>(diff);
+    // This is an approximate size
+    auto tail = tail_.load(std::memory_order_acquire);
+    auto head = head_.load(std::memory_order_acquire);
+    auto maxToTail = MAX_SIZE_T - tail;
+    // This needs to be a signed type, and causes a narrowing of size_t
+    std::ptrdiff_t headToTail = head - tail;
+    // Handles wrap around case
+    return (maxToTail < std::abs(headToTail)) ? maxToTail + head : headToTail;
   }
 
   [[nodiscard]] bool empty() const noexcept
   {
-    return head_.load(std::memory_order_acquire) ==
-           tail_.load(std::memory_order_acquire);
+    return size() <= 0;
   }
 
-  [[nodiscard]] std::size_t capacity() const noexcept { return capacity_ - 1; }
+  [[nodiscard]] std::size_t capacity() const noexcept { return capacity_; }
 };
 }// namespace dro
 #endif
